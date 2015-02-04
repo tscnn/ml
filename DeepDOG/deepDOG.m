2ad12a207ebbc46c7b82f56af3e0d5092fb746d2
tscnn <tobias.schomann@gmail.com>
2015-02-04 14:16:26 +0100

    ...

--- DeepDOG/deepDOG.m
+++ DeepDOG/deepDOG.m
@@ -1,17 +1,13 @@
-function [ imgnone, imgmax, imgsquare ] = deepDOG( img, epochs )
+function [ imgnone, imgmax, imgsquare ] = deepDOG( img, epochs, k, sigma, hsize )
 
 imgnone = img;
 imgmax = img;
 imgsquare = img;
 
 for layer = 1:epochs
-    
-    imgnone = dog(imgnone);
-    
-    imgmax = max(dog(imgmax),0);
-    
-    imgsquare = (dog(imgsquare)).^2;
-    
+    imgnone = dog(imgnone, k, sigma, hsize);
+    imgmax = max(dog(imgmax, k, sigma, hsize),0);
+    imgsquare = (dog(imgsquare, k, sigma, hsize)).^2;
 end
 
 end
--- DeepDOG/dog.m
+++ DeepDOG/dog.m
@@ -1,25 +1,29 @@
-function [ dog ] = dog( img, k, sigma, hsize )
+function [ dimg ] = dog( img, k, sigma1, hsize )
+% based on http://stackoverflow.com/questions/20057146/is-there-any-difference-of-gaussians-function-in-matlab
 
 if nargin < 4
     hsize = [3 3];
 end
 
 if nargin < 3
-    sigma = 0.5;
+    sigma1 = 0.5;
 end
 
 if nargin < 2
     k = 10;
 end
 
-sigma2 = sigma * k;
+sigma2 = sigma1 * k;
 
-h1 = fspecial('gaussian', hsize, sigma);
+h1 = fspecial('gaussian', hsize, sigma1);
 h2 = fspecial('gaussian', hsize, sigma2);
 
 gauss1 = imfilter(img,h1,'replicate');
 gauss2 = imfilter(img,h2,'replicate');
 
-dog = gauss1 - gauss2;
+dimg = gauss1 - gauss2;
+
+% normalization
+dimg = dimg / max(max(dimg));
 
 end
